712f56daa8975367eb68d6d47df6156ddac3449f
Vavrines <tianbaixiao@gmail.com>
2020-05-26 10:59:04 +0200

    Add dependencies

--- src/SKS.jl
+++ src/SKS.jl
@@ -13,10 +13,10 @@ using FileIO
 using JLD2
 using Plots
 #using DifferentialEquations
-#using Kinetic
 
-include("D:\\Coding\\Github\\Kinetic.jl\\src\\Kinetic.jl")
-@reexport using .Kinetic
+@reexport using Kinetic
+#include("D:\\Coding\\Github\\Kinetic.jl\\src\\Kinetic.jl")
+#@reexport using .Kinetic
 
 include("uq.jl")
 include("kinetic.jl")
