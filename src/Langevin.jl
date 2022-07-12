252474e2e9d5a9f2a5e7ac948293fa244e336ac1
vavrines <tianbaixiao@gmail.com>
2022-07-12 16:25:10 +0200

    Format codes

--- src/Langevin.jl
+++ src/Langevin.jl
@@ -1,30 +1,32 @@
 """
 Stochastic Scheme for Uncertainty Quantification
+
 Copyright (c) 2020-2022 Tianbai Xiao
 """
 
 module Langevin
 
 using Reexport
-@reexport using PolyChaos
 @reexport using KitBase
-using Base.Threads: @threads
+@reexport using PolyChaos
 using KitBase.FiniteMesh.DocStringExtensions
 using KitBase.JLD2
 using KitBase.OffsetArrays
+using Base.Threads: @threads
 
-export AbstractUQ, UQ1D, UQ2D, ran_chaos, chaos_ran, lambda_tchaos, t_lambdachaos
+export AbstractUQ, UQ1D, UQ2D
+export ran_chaos, chaos_ran, lambda_tchaos, t_lambdachaos
 export uq_moments_conserve,
-       uq_maxwellian,
-       uq_energy_distribution,
-       uq_prim_conserve,
-       uq_conserve_prim,
-       uq_conserve_prim!,
-       uq_prim_conserve!,
-       uq_sound_speed,
-       uq_vhs_collision_time,
-       uq_aap_hs_collision_time,
-       uq_aap_hs_prim
+    uq_maxwellian,
+    uq_energy_distribution,
+    uq_prim_conserve,
+    uq_conserve_prim,
+    uq_conserve_prim!,
+    uq_prim_conserve!,
+    uq_sound_speed,
+    uq_vhs_collision_time,
+    uq_aap_hs_collision_time,
+    uq_aap_hs_prim
 
 include("struct.jl")
 include("uq.jl")
