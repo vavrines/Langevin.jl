cf8b5dd2d2d9c24ba7d670832f85fcd05efbc773
vavrines <tianbaixiao@gmail.com>
2022-07-22 09:56:00 +0200

    Clean annotation

--- src/Langevin.jl
+++ src/Langevin.jl
@@ -13,6 +13,7 @@ using KitBase.FiniteMesh.DocStringExtensions
 using KitBase.JLD2
 using KitBase.LinearAlgebra
 using KitBase.OffsetArrays
+using KitBase: AV, AM, AA
 using Base.Threads: @threads
 
 export LV
